617352518dcf8b0c1e8fd6c32519f781f144498d
nhuthq <nhuthq@zigexn.vn>
2024-11-13 13:21:24 +0700

    finish section

--- interno/src/App.jsx
+++ interno/src/App.jsx
@@ -42,6 +42,7 @@ const App = () => {
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 };
@@ -87,7 +88,6 @@ const Main = () => {
       <StatsSection />
       <NewsSection />
       <ContactSection />
-      <div className="h-[3000px] bg-slate-300"></div>
     </main>
   );
 };
@@ -498,6 +498,87 @@ const ContactSection = () => {
         </div>
       </div>
     </section>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer
+      id="footer"
+      className="footer relative mt-[50px] xl:mt-[100px] mx-12 z-20"
+    >
+      <div className="flex flex-col xl:flex-row gap-26 xl:gap-36 mx-auto ">
+        <div className="w-full w-max-[400px] flex flex-col items-center xl:items-start space-y-8 text-center xl:text-start mb-8">
+          <a>
+            <img src={logo} alt="logo" />
+          </a>
+          <p className="text-xl">
+            It is a long established fact that a reader will be distracted
+            lookings.
+          </p>
+          <ul className="text-primary flex flex-row gap-14 justify-center items-center xl:justify-start ">
+            <li>
+              <a href="#">
+                <i className="ri-facebook-fill"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="ri-twitter-fill"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="ri-linkedin-fill"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="ri-instagram-fill"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-full flex flex-col xl:flex-row text-center xl:text-start gap-12 xl:gap-28 justify-center xl:justify-end">
+          {/* Pages */}
+          <div className="foooter__item">
+            <h3 className="h3 mb-3 ">Pages</h3>
+            <ul className="flex flex-col gap-4">
+              <li>About Us</li>
+              <li>Testimonials</li>
+              <li>Our Works</li>
+              <li>News</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          {/* Services */}
+          <div className="foooter__item">
+            <h3 className="h3 mb-3 ">Services</h3>
+            <ul className="flex flex-col gap-4">
+              <li>Kitchen</li>
+              <li>Living Area</li>
+              <li>Bathroom</li>
+              <li>Bedroom</li>
+            </ul>
+          </div>
+
+          {/* Contact */}
+          <div className="foooter__item max-w-[260px] mx-auto xl:mx-0">
+            <h3 className="h3 mb-3 ">Contact</h3>
+            <ul className="flex flex-col gap-6 text-xl">
+              <p>55 East Birchwood Ave. Brooklyn, New York 11201</p>
+              <p>contact@interno.com</p>
+              <p>(123) 456 - 7890</p>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <p className="footer_copyright mt-12 p-12 text-xl text-center xl:border-t bg-white">
+        Copyright © Interno 2024. All rights reserved.
+      </p>
+    </footer>
   );
 };
 
